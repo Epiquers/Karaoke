669aa4fb84de911a2cbbd3cfb58385d5966d7c47
Eduardo Piquer Sanchez <eduardo.piquer.sanchez@ces-vegamedia.es>
2026-05-27 16:41:33 +0200

    Pruebas responsive

--- administrador/perfil_admin.php
+++ administrador/perfil_admin.php
@@ -41,6 +41,7 @@ if (isset($_POST['accion_estado'])) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kantabile - Gestión de Usuarios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
